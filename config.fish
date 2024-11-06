76c08a954aa61233d5c52e6efef26cf02e6f16ff
Jamie Fairhurst <37811269+jami3f@users.noreply.github.com>
2024-11-06 12:23:52 +0000

    First commit

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+abbr -a l ls -la
+
+# Created by `pipx` on 2024-11-05 17:16:41
+set PATH $PATH $HOME/.local/bin
