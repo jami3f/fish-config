11e09d9796971d972f8b0e5fcded82a3ba8cf062
Jamie Fairhurst <jamie.fairhurst01@gmail.com>
2024-11-16 18:56:55 +0000

    Added function for clonedev and added completions for docker and pipx

--- config.fish
+++ config.fish
@@ -5,3 +5,6 @@ abbr -a l ls -la
 
 # Created by `pipx` on 2024-11-05 17:16:41
 set PATH $PATH /Users/James.Fairhurst@computacenter.com/.local/bin
+
+# Created by `pipx` on 2024-11-06 13:01:42
+set PATH $PATH /home/jamie/.local/bin
