55ea3c0a354d87df3340768602e50148378b216b
Jamie Fairhurst <37811269+jami3f@users.noreply.github.com>
2025-01-28 18:21:32 +0000

    Set colours

--- config.fish
+++ config.fish
@@ -2,6 +2,12 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
+# Tide config settings
+set tide_left_prompt_items context pwd character
+set tide_right_prompt_items status cmd_duration jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+set tide_context_color_ssh 73C0E2
+set tide_context_color_root EA4649
+
 # Abbreviations
 abbr -a l ls -la
 
@@ -24,6 +30,7 @@ set PATH $PATH /Users/James.Fairhurst@computacenter.com/.local/bin
 # Created by `pipx` on 2024-11-06 13:01:42
 set PATH $PATH /home/jamie/.local/bin
 set PATH $PATH $HOME/.local/bin
+
 if type -q zoxide
 zoxide init fish | source
 end
